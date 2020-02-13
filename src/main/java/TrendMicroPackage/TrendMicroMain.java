3c800944f4b228322282cfeff098c616ba130367
Sprrwkk <sprrwkk@gmail.com>
2020-02-13 12:04:54 +0100

    Import some libraries

--- src/main/java/TrendMicroPackage/TrendMicroMain.java
+++ src/main/java/TrendMicroPackage/TrendMicroMain.java
@@ -1,14 +1,15 @@
 package TrendMicroPackage;
+
+import java.io.File;
+import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class TrendMicroMain {
 
     public static void main(String[] args) {
-
 
 
 
