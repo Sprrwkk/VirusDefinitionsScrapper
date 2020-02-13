e36ee44da7b0dafb4f0f08f5cb5aa7e83594b490
Sprrwkk <sprrwkk@gmail.com>
2020-02-13 14:35:28 +0100

    Create TrendMicro virus definition file download, read and save functionality.

--- src/main/java/TrendMicroPackage/TrendMicroMain.java
+++ src/main/java/TrendMicroPackage/TrendMicroMain.java
@@ -1,17 +1,104 @@
 package TrendMicroPackage;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipFile;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 public class TrendMicroMain {
 
-    public static void main(String[] args) {
+    static String fileName;
+    static String fileSize;
+
+    public static void main(String[] args) throws IOException{
+          downloadZipFile();
+          readFileInfo();
+          saveInfoToFile();
+    }
 
 
+
+    /**
+     *Function that connect to download center and create file path to zip file.
+     * @return currentFilePath
+     * @throws IOException
+     */
+
+    public static String getFilePathUrl() throws IOException {
+
+        Document doc;
+
+        // Filepath to main download center
+        String downloadCenterPath = "https://downloadcenter.trendmicro.com/index.php?clk=tab_pattern&clkval=1&regs=nabu&lang_loc=1";
+        // connect to main download center
+        doc = Jsoup.connect(downloadCenterPath).get();
+
+        // query <h2> - this tag store name of current file
+        fileName = doc.select("h2").text().substring(31, 34);
+
+        // create new downloading path with current file
+        String currentFilePath = "https://www.trendmicro.com/ftp/products/aupattern/ent95/lpt" + fileName + ".zip";
+
+        return currentFilePath;
+    }
+
+
+
+    /**
+     *Function that download a zip file by getting url from getFilePathUrl function.
+     * @throws IOException
+     */
+
+    public static void downloadZipFile() throws IOException {
+
+        String saveFilePath = "C:\\Users\\lwrobel\\IdeaProjects\\VirusScrapper\\Files\\";
+        // Create InputSteam object for download file
+        InputStream in = URI.create(getFilePathUrl()).toURL().openStream();
+
+        // Saved file to Files dir path with replace existing option
+        Files.copy(in, Paths.get(saveFilePath + "lpt$vpn" + fileName + ".zip"),
+                StandardCopyOption.REPLACE_EXISTING);
+        in.close();
+    }
+
+
+
+    /**
+     * Function that read file information and save it (file name and file size) to static's variables
+     * @throws IOException
+     */
+
+    public static void readFileInfo() throws IOException {
+        ZipFile trendMicroZipFile = new ZipFile("C:\\Users\\lwrobel\\IdeaProjects\\VirusScrapper\\Files\\" +
+                "lpt$vpn" + fileName + ".zip");
+
+        long size = trendMicroZipFile.entries().nextElement().getSize();
+        fileSize = Long.toString(size);
+    }
+
+
+    
+    /**
+     * Function that save information stored in static's variables to filename.txt in Files dir
+     * @throws IOException
+     */
+    public static void saveInfoToFile() throws IOException {
+        File filePath = new File("C:\\Users\\lwrobel\\IdeaProjects\\VirusScrapper\\Files\\" + "lpt$vpn" + fileName + ".txt");
+
+        // write to file
+        FileWriter fw = new FileWriter(filePath, false);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("lpt$vpn" + fileName);
+        bw.write("\n");
+        bw.write(fileSize);
+        bw.close();
+        System.out.println("Write:" + "lpt$vpn" + fileName);
+        System.out.println(fileSize);
 
     }
 }
